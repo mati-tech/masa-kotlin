56f75abbe4bb64fd4ec08355ccc34299d96b813e
Matiullah Aziz <azizmatiullah@yahoo.com>
2024-09-17 22:42:07 +0300

    added some initial files from masa java to kotlin

--- app/src/main/java/com/mati_tech/masa_english_learning/ui/activities/MainActivity.kt
+++ app/src/main/java/com/mati_tech/masa_english_learning/ui/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.mati_tech.masa_english_learning
+package com.mati_tech.masa_english_learning.ui.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mati_tech.masa_english_learning.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
